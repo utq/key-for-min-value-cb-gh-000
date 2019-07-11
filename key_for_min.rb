421077b339457c788b17c6f21819c6e60849be6d
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 13:30:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = 99999999999999
+  name_hash.collect do |name, num|
+    if num < min
+      min = num
+    end
+  end
+    if min = 99999999999999
+      return nil
+    end
 end

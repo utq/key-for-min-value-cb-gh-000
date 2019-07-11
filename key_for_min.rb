b82fe465e362e23beacfb59308c5febba6e4fa24
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 13:31:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,16 @@
 
 def key_for_min_value(name_hash)
   min = 99999999999999
+  nameA = ""
   name_hash.collect do |name, num|
     if num < min
       min = num
+      nameA = name
     end
   end
     if min = 99999999999999
       return nil
+    else
+      return name
     end
 end
